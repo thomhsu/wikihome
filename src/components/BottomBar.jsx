9691fe352d676f3bacdf0dcc1e339b05fa1c96c9
Thomas Hsu <thomhsu@gmail.com>
2019-03-16 15:35:05 -0700

    Add modal

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { 
   Menu,
+  Modal,
   Container,
   Button,
   Form,
@@ -15,20 +16,34 @@ import {
 function BottomBar() {
 
   const [topics, setTopics] = useState([]);
+  const [open, newTopicOpened] = useState(false);
 
   return (
     <Menu
       className='inverted bottom fixed'
     >
       <Menu.Item
-        name='search'>
+        name='search'
+      >
         <Search />
       </Menu.Item>
       <Menu.Item
-        name='newTopic'>
+        name='newTopic'
+        onClick={() => newTopicOpened(true)}  
+      >
         Add New Topic  
       </Menu.Item>
 
+      <Modal size='fullscreen' open={open} onClose={() => newTopicOpened(false)}>
+        <Modal.Header>Delete Your Account</Modal.Header>
+        <Modal.Content>
+          <p>Are you sure you want to delete your account</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button negative>No</Button>
+          <Button positive icon='checkmark' labelPosition='right' content='Yes' />
+        </Modal.Actions>
+      </Modal>
     </Menu>
   );
 }
